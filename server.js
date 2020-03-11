4dcd18a77462343ceb8ef3cce08ef9d8176c0e98
Jay Glickman <jayglickman@gmail.com>
2020-03-11 16:20:01 -0400

    more heroku

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.use(session({
 
 // Connect mongoose to mongo db's:
 let uristring = 
-process.env.MONGOLAB_URI || process.env.MONGOHQ_URL
+process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/study_guide';
 
 mongoose.connect(uristring, (err, res) => {
   if (err) {
-    console.log ("ERROR connecting to: " + uristring + ". " + err + " Process.env.MONGOLAB_URI is " + process.env.MONGODB_URI + " Process.env.MONGOHQ_URL is " + process.env.MONGOHQ_URL);
+    console.log ("ERROR connecting to: " + uristring + ". " + err + " Process.env.MONGOLAB_URI is " + process.env.MONGODB_URI + " Process.env.MONGOHQ_URL is " + process.env.MONGOHQ_URL) + " uristring is " + uristring;
   } else {
     console.log("Succedded connected to: " + uristring)
   }
